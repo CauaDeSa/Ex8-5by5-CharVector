e0be2aa565c3c862a88d93935d4540f32e77c5c4
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-22 14:29:57 -0300

    Add project files.

---
+++ Ex8-5by5-CharVector/Program.cs
@@ -0,0 +1,29 @@
+﻿int size = 10;
+char[] palavra = new char[size];
+
+Console.WriteLine("\nCharactere input");
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"\nType the {i + 1}° charactere: ");
+    palavra[i] = char.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nVowels");
+
+for (int i = 0; i < size; i++)
+{
+    if (palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+        Console.Write($"{palavra[i]} ");
+}
+
+Console.WriteLine("\n\nConsonants");
+
+for (int i = 0; i < size; i++)
+{
+    if (palavra[i] != 'a' && palavra[i] != 'e' && palavra[i] != 'i' && palavra[i] != 'o' && palavra[i] != 'u')
+        Console.Write($"{palavra[i]} ");
+}
+
+Console.WriteLine("\n\nPress any key to continue...");
+Console.ReadKey();
